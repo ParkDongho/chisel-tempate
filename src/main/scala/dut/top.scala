3fed5f52c1b6a573e6988f712c2ba5c97d19e863
Dongho Park <parkdongho28@naver.com>
2023-04-26 18:23:38 +0900

    Update top.scala

--- src/main/scala/dut/top.scala
+++ src/main/scala/dut/top.scala
@@ -2,41 +2,8 @@ package arithmetics
 
 import chisel3._
 
-class AdderIO(width: Int) extends Bundle{
-  //input
-  val in = new Bundle{
-    val carry = Input(Bool())
-    val a = Input(UInt(width.W))
-    val b = Input(UInt(width.W))
-  }
-
-  //output
-  val out = new Bundle{
-    val carry = Output(Bool())
-    val z = Output(UInt(width.W))
-  }
+class Top extends Module{
+  
 }
 
-/**
- * Reference Adder
- */
-class ReferenceAdder extends Module{
-  val io = IO(new AdderIO(8))
-  io.out := (io.in.a +& io.in.b + io.in.carry.asUInt).asTypeOf(io.out)
-}
-
-/**
- * Fast Carry
- */
-class RippleCarryAdder extends Module{
-  val io = IO(new AdderIO(8))
-}
-
-class CarryLookAheadAdder extends Module{
-  val io = IO(new AdderIO(8))
-}
-
-class CarrySkipAdder extends Module{
-  val io = IO(new AdderIO(8))
-}
 
